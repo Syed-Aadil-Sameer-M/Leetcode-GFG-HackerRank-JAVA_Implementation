420d6d3eee389e5f06537bb85bca2e19056593da
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-05-16 14:55:18 +0530

    Time: 0 ms (100%), Space: 43.1 MB (94.29%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
