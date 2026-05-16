a02bf6485411164fdcb81c3ebe1b5240bfd952ae
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-05-16 15:48:01 +0530

    Added solution - LeetHub

---
+++ 701389-count-digits-in-a-number-GFG/701389-count-digits-in-a-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int countDigits(int n) {
+        // Math.log10(n) gives the power of 10. 
+        // For example, Math.log10(1567) is ~3.195. Taking the floor gives 3, plus 1 equals 4.
+        return (int) Math.floor(Math.log10(n)) + 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
