a5884efa1a45ef49c8b0c2ae8cc2b3fb57fefb2f
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-05-16 16:06:58 +0530

    Added solution - LeetHub

---
+++ 901725-longest-periodic-proper-prefix-GFG/901725-longest-periodic-proper-prefix.java
@@ -0,0 +1,36 @@
+class Solution {
+    int getLongestPrefix(String s) {
+        int n = s.length();
+        if (n <= 1) return -1;
+
+        int[] z = new int[n];
+        int l = 0, r = 0;
+
+        // Construct Z-array in O(n)
+        for (int i = 1; i < n; i++) {
+            if (i <= r) {
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            }
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+
+        // Search from longest proper prefix (n-1) down to 1
+        for (int len = n - 1; len >= 1; len--) {
+            if (z[len] == n - len) {
+                return len;
+            }
+        }
+
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
