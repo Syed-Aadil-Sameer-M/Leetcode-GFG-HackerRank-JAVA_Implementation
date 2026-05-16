c9dcf489abb5cbdb90071e300312da5848f82538
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-05-16 14:48:34 +0530

    Time: 0 ms (100%), Space: 43.6 MB (90.47%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // Binary search on rotated sorted array
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Determine which side is properly sorted
+            if (nums[left] <= nums[mid]) { // left side sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // right side sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1; // not found
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
