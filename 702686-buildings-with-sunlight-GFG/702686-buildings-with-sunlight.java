6d7a3c2380bf3fec1a8adaa768910c40708e0f9e
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-06-05 09:30:32 +0530

    Added solution - LeetHub

---
+++ 702686-buildings-with-sunlight-GFG/702686-buildings-with-sunlight.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int visibleBuildings(int arr[]) {
+        int count = 0;
+        int maxHeight = Integer.MIN_VALUE;
+
+        for (int height : arr) {
+            if (height >= maxHeight) {
+                count++;
+                maxHeight = height;
+            }
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
