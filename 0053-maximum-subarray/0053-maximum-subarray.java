b7a6d8da498fe7a62723871b733f7ab87106d02d
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-08-01 17:26:50 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (66.81%) - LeetHub - Time: O(n), Space: O(1)

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // Kadane's algorithm: maintain current subarray sum and global maximum.
-        int maxSoFar = nums[0];
-        int curr = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            curr = Math.max(nums[i], curr + nums[i]);
-            maxSoFar = Math.max(maxSoFar, curr);
+        int n = nums.length;
+        int sum = nums[0];
+        int cur = nums[0];
+        if(n==0){
+            return 0;
         }
-        return maxSoFar;
+        if(n==1){
+            return nums[0];
+        }
+        if(n>1){
+            for(int i=1; i<n; i++){
+                cur = Math.max(nums[i], nums[i]+cur);
+                sum = Math.max(sum, cur);
+            }
+            return sum;
+        }
+        return 0;
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
